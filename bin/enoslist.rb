15f472f289a386ae5cd40ca36c89a1c63da75859
QuentinT <quentintual2@gmail.com>
2023-02-01 11:40:24 +0100

    Task listed.

--- bin/enoslist.rb
+++ bin/enoslist.rb
@@ -1,1 +1,30 @@
-# TODO : Application en ligne de commande.
+require 'optparse'
+require "../lib/netlist.rb"
+
+@options = {}
+args = {}
+
+OptionParser.new do |opts|
+
+    Version = "Enoslist 0.1.0a (Feb 2023)"
+
+    opts.on("-v", "--verbose", "Show extra information.") do
+        @options[:verbose] = true
+    end
+
+    opts.on("-V", "--version", "Show used program current version.") do
+        puts Version
+    end
+
+    # TODO : Ajouter une option pour le passage d'un JSON à la netlist
+
+    # TODO : Ajouter une option pour le passage d'une netlist au JSON
+
+    # TODO : Ajouter une option pour la converion netlist vers xdot
+
+    # TODO : Ajouter une option pour la sérialisation (export) de la netlist
+
+    # TODO : Ajouter une option pour l'import de la netlist sérialisée
+    
+end.parse!
+
--- lib/netlist/circuit.rb
+++ lib/netlist/circuit.rb
@@ -1,57 +1,5 @@
+require_relative 'port.rb'
 module Netlist
-
-    class Port
-        attr_accessor :name, :partof, :fanin, :fanout, :direction
-
-        def initialize name, direction
-            @name = name
-            @partof = nil
-            @direction = direction
-            @fanin = nil # Always only one source to the input 
-            @fanout = []
-        end
-
-        def <=(e) 
-            if (e.direction == self.direction and (e.partof != nil and self.partof != nil)) 
-                puts "Warning : Global IO wiring detected. Possible unwanted wiring detected between #{e.name} and #{self.name}, same direction ports wired."
-                global_wiring = true
-            end
-
-            case e.direction
-            when :in
-                wire e
-            when :out 
-                if global_wiring
-                    wire e
-                else
-                    raise "Error : Wiring can only be applied from output to an input except for global IOs. Please verify."
-                end
-            end
-        end
-
-        def wire e
-            @fanout << e        # Many input might be connected to one output 
-            if (e.fanin != nil and e.fanin.class != String)
-                puts "Warning : Input port #{e.name} already wired, input source will be replaced" 
-            end
-            e.fanin = self     # Only one ouput connected to an input
-        end
-
-        def to_hash uid_table
-            uid_table << self.object_id
-            {
-                :class => self.class.name,
-                :data =>    {   
-                                :name => @name, 
-                                :partof => (@partof == nil ? nil : @partof.name), 
-                                :direction => @direction, 
-                                :fanin => (@fanin == nil ? nil : @fanin.name),
-                                :fanout => @fanout == [] ? nil : @fanout.collect!{|e| e.name}
-                            }
-            }
-        end
-    end
-
     class Circuit
         attr_accessor :name, :ports, :components, :partof
 
