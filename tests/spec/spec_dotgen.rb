6dd5804a28d1ebd4d7e4e88a3144115ecc7823a0
QuentinT <quentintual2@gmail.com>
2023-02-01 11:43:26 +0100

    Tasks listed.

---
+++ tests/spec/spec_dotgen.rb
@@ -0,0 +1,9 @@
+require "../lib/netlist.rb"
+
+Rspec.describe Netlist::Netson do
+
+    # TODO : Vérifier la validité de la syntaxe du dot en sortie
+
+    # TODO : Vérifier le contenu (devrait contenir certains éléments, pas forcément exhaustif, plutôt par mot clé)
+
+end
---
+++ tests/spec/spec_netson.rb
@@ -0,0 +1,13 @@
+require "../lib/netlist.rb"
+
+Rspec.describe Netlist::Netson do
+
+    # TODO : Vérifier l'import du format JSON 
+
+    # TODO : Vérifier la conversion JSON vers netlist
+
+    # TODO : Vérifier l'export de la netlist au format JSON
+
+    # TODO : Vérifier 
+
+end
