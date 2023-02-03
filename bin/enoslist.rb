6e42e14c2751db4c328486fd1ab7a39c2ba7cbdb
QuentinT <quentintual2@gmail.com>
2023-02-03 17:49:36 +0100

    Options added, thinking on the interface.

--- bin/enoslist.rb
+++ bin/enoslist.rb
@@ -1,8 +1,8 @@
 require 'optparse'
-require "../lib/netlist.rb"
+require_relative "../lib/netlist.rb"
 
 @options = {}
-args = {}
+@args = {}
 
 OptionParser.new do |opts|
 
@@ -16,15 +16,30 @@ OptionParser.new do |opts|
         puts Version
     end
 
+    # TODO : Ajouter une option pour l'import de la netlist sérialisée
     # TODO : Ajouter une option pour le passage d'un JSON à la netlist
+    # ! : Pour le format, voir en ajoutant une autre option, plus simple en revanche serait préférable en ayant un requirement sur les options d'import ou d'export pour que la commande de format ait un sens.
+    opts.on("-i FORMAT,FILE", "--import=FORMAT,FILE1,FILE2,...", Array, "Import a Netlist from one to multiple files in a specified format.") do |passed_args|
+        @options[:import] = true
+        @args[:import] = {:format => passed_args[0]}
+        passed_args.shift 
+        @args[:import][:files] = passed_args
+    end
 
     # TODO : Ajouter une option pour le passage d'une netlist au JSON
+    opts.on("-e FILE,FORMAT", "--export=FILE,FORMAT", Array, "Export a Netlist to a specified format in a file with the given name.") do |passed_args|
+        @options[:export] = true
+        @args[:export] = {:format => passed_args[1], :file => passed_args[0]}
+        # TODO : Ajouter une option pour la sérialisation (export) de la netlist -> Un export avec marshal se fera via une valeur 'nil' par défaut
+    end
 
     # TODO : Ajouter une option pour la converion netlist vers xdot
+    opts.on("-s", "--show", "Export the current Netlist to .dot format.") do
+        @options[:show] = true
+        # ? Voir si on ajoute un nom ou pas pour préciser l'entité à visualiser, sûrement plus pratique
+    end
 
-    # TODO : Ajouter une option pour la sérialisation (export) de la netlist
-
-    # TODO : Ajouter une option pour l'import de la netlist sérialisée
-    
 end.parse!
+
+pp @args
 
