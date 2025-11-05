8766db61f7dc2866abbde668ff173afd04b5ce9a
QuentinT <quentintual2@gmail.com>
2025-11-05 15:22:20 +0100

    update gem and clean

--- compile.sh
+++
@@ -1,4 +0,0 @@
-ghdl -a --std=08 --work=test_lib -P=. test.vhd
-ghdl -a --std=08 --work=test_lib -P=. test_1_tb.vhd
-ghdl --elab-run --std=08 --work=test_lib -P=. test_1_tb --read-wave-opt=test_1_tb.opt
- 
