aaf44cda24cd6e22da2f4876c0658d282833235c
QuentinT <quentintual2@gmail.com>
2026-06-19 14:42:40 +0200

    linked librelane env functionnality

---
+++ lib/librelane.rb
@@ -0,0 +1,5 @@
+require_relative 'librelane/blif_renamer'
+require_relative 'librelane/ateta_lblane'
+require_relative 'librelane/htpg_lblane'
+
+require_relative 'librelane/librelane_env'
--- lib/librelane/librelane.rb
+++
@@ -1,4 +0,0 @@
-require_relative 'blif_renamer'
-require_relative 'ateta_lblane'
-
-require_relative 'librelane_env'
